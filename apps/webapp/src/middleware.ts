a4f64ce9c65c01a7b36f5f2c861e7ed9e8570444
Anas Barghoud <abarghoud@gmail.com>
2026-07-18 22:48:08 +0200

    fix(webapp): update Content Security Policy for Crisp Chat and Buy Me a Coffee

--- apps/webapp/src/middleware.ts
+++ apps/webapp/src/middleware.ts
@@ -10,7 +10,7 @@ export function middleware(request: NextRequest): NextResponse {
 
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
 
-  const cspHeader = `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.buymeacoffee.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://img.buymeacoffee.com; connect-src 'self' ${apiUrl} https://api.tesla.com https://api.rollbar.com; frame-ancestors 'none';`;
+  const cspHeader = `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.buymeacoffee.com https://client.crisp.chat; style-src 'self' 'unsafe-inline' https://client.crisp.chat; img-src 'self' data: blob: https://img.buymeacoffee.com https://cdn.buymeacoffee.com https://client.crisp.chat https://image.crisp.chat; connect-src 'self' ${apiUrl} https://api.tesla.com https://api.rollbar.com https://client.crisp.chat wss://client.relay.crisp.chat wss://stream.relay.crisp.chat; worker-src 'self' blob:; frame-src 'self' https://*.crisp.help https://www.buymeacoffee.com https://buymeacoffee.com; font-src 'self' https://client.crisp.chat; frame-ancestors 'none';`;
 
   response.headers.set('Content-Security-Policy', cspHeader);
 
